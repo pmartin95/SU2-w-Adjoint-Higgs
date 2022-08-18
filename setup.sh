8a9e096cd5c73f7213812b939e5cd44a24d45490
pmartin95 <martip8@rpi.edu>
2022-08-18 13:27:53 -0400

    add job files

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+
+mkdir -p bin obj dat configurations
+make all -j
---
+++ submitjob1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sbatch -p RM --time 2:00:00\
+--mail-type=ALL --mail-user=martip8@rpi.edu\
+-A tra160032p -o output/job1.output -e job1.err -J "775p0"\
+--ntasks=1 ./job1.job 
