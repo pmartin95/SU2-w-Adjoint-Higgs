d7ce5f7d4eba44ae2e3a9b1396ede137cd0ccd76
pmartin95 <pmartin95.phy@gmail.com>
2022-08-22 12:44:13 -0500

    fix index

--- src/configuration_io.cpp
+++ src/configuration_io.cpp
@@ -1,23 +1,24 @@
 #include "configuration_io.hpp"
 #include "global_decl.hpp"
 #include "generic_func.hpp"
+#include <fstream>
 
 int pushConfig(std::string filename)
 {
     std::vector<double> v;
     std::ofstream conf;
-    for(int site_index = 0;i<site_index;site_index++)
+    for(int site_index = 0;site_index<lsites;site_index++)
     {
         for(int dir = 0; dir < 4;dir++)
         {
-            v.push_back(lattice[site_index].field[i](0, 0).real());
-            v.push_back(lattice[site_index].field[i](0, 1).imag());
-            v.push_back(lattice[site_index].field[i](0, 1).real());
-            v.push_back(lattice[site_index].field[i](1, 0).imag());
-            v.push_back(lattice[site_index].field[i](1, 0).real());
-            v.push_back(lattice[site_index].field[i](0, 0).imag());
-            v.push_back(lattice[site_index].field[i](1, 1).imag());
-            v.push_back(lattice[site_index].field[i](1, 1).real()); 
+            v.push_back(lattice[site_index].field[dir](0, 0).real());
+            v.push_back(lattice[site_index].field[dir](0, 1).imag());
+            v.push_back(lattice[site_index].field[dir](0, 1).real());
+            v.push_back(lattice[site_index].field[dir](1, 0).imag());
+            v.push_back(lattice[site_index].field[dir](1, 0).real());
+            v.push_back(lattice[site_index].field[dir](0, 0).imag());
+            v.push_back(lattice[site_index].field[dir](1, 1).imag());
+            v.push_back(lattice[site_index].field[dir](1, 1).real()); 
         }
     }
     conf.open(filename, std::ios::binary);
@@ -30,17 +31,17 @@ int pullConfig(std::string filename)
     int i, j, k;
     int doublesPerSite = 4 /* matrices per site */ * 8 /*doubles per matrix*/;
     std::ifstream conf;
-    std::vector<double> v(nsites * doublesPerSite);
+    std::vector<double> v(lsites * doublesPerSite);
 
     conf.open(filename, std::ios::binary);
-    conf.read(reinterpret_cast<char *>(&v[0]), nsites * doublesPerSite * sizeof(double));
+    conf.read(reinterpret_cast<char *>(&v[0]), lsites * doublesPerSite * sizeof(double));
     conf.close();
 
-    for (unsigned long int site_index = 0; site_index < nsites; site_index++)
+    for ( int site_index = 0; site_index < lsites; site_index++)
     {
         k = site_index * doublesPerSite;
         j = 0;
-        FORALLDIR(i)
+        for(int i =0;i<4;i++)
         {
             lattice[site_index].field[i](0, 0) = std::complex<double>(v[k + j++], v[k + j++]);
             lattice[site_index].field[i](0, 1) = std::complex<double>(v[k + j++], v[k + j++]);
