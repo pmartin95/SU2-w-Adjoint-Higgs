d22a0b3ebf609f9940c97419c4915ffb4d9954a9
pmartin95 <pmartin95.phy@gmail.com>
2022-08-24 11:39:42 -0500

    change ntasks

--- submitjob1.sh
+++ submitjob1.sh
@@ -2,4 +2,4 @@
 sbatch -p RM -t 02:00:00 \
 --mail-type=ALL --mail-user=martip8@rpi.edu \
 -A tra160032p -o output/job1.output -e job1.err -J "775p0" \
---ntasks=1 ./job1.job 
+--ntasks=39 ./job1.job 
