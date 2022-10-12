9e927888ef223bc79f3d2b9161abf193b6f90e51
pmartin95 <martip8@rpi.edu>
2022-10-12 16:17:03 -0400

    double check action

--- src/action.cpp
+++ src/action.cpp
@@ -56,42 +56,40 @@ double GeorgiGlashowHiggsPotentialSite(int site_index)
     return tmp * tmp * lambda + tmp * m2;
 }
 
+//! Investigate this term
 double GeorgiGlashowHiggsKinetic()
 {
-    double accumulator = 0.0, internalAccumulator = 0.0;
+    double accumulator = 0.0;
     matrix tmp;
     for (int site_index = 0; site_index < lsites; site_index++)
     {
         tmp = lattice[site_index].field[4];
         accumulator += 4.0 * (tmp * tmp).trace().real();
-        internalAccumulator = 0.0;
         for (int dir = 0; dir < 4; dir++)
         {
-            internalAccumulator += HiggsMixedTerm(site_index, dir);
+            accumulator -= HiggsMixedTerm(site_index, dir);
         }
-        accumulator -= internalAccumulator;
     }
     return 2.0 * kappa * accumulator;
 }
-
+//! Investigate this term
 double GeorgiGlashowHiggsKineticSite(int site_index)
 {
-    double accumulator, internalAccumulator;
+    double accumulator;
     matrix tmp = lattice[site_index].field[4];
     accumulator = 4.0 * (tmp * tmp).trace().real();
-    internalAccumulator = 0.0;
+
     for (int dir = 0; dir < 4; dir++)
     {
         int x[4];
         siteIndexToCoordinates(site_index, x[0], x[1], x[2], x[3]);
         x[dir] = (x[dir] - 1 + ldir[dir]) % ldir[dir];
-        internalAccumulator += HiggsMixedTerm(site_index, dir);
-        internalAccumulator += HiggsMixedTerm(coordinatesToSiteIndex(x[0], x[1], x[2], x[3]), dir);
+        accumulator -= HiggsMixedTerm(site_index, dir);
+        accumulator -= HiggsMixedTerm(coordinatesToSiteIndex(x[0], x[1], x[2], x[3]), dir);
     }
-    accumulator -= internalAccumulator;
     return 2.0 * kappa * accumulator;
 }
-
+//! Investigate this term
 double GeorgiGlashowHiggsKineticSiteOneLink(int site_index, int dir)
 {
     return -2.0 * kappa * HiggsMixedTerm(site_index, dir);
@@ -109,7 +107,7 @@ double GeorgiGlashowPartialActionHiggs(int site_index)
 {
     return GeorgiGlashowHiggsKineticSite(site_index) + GeorgiGlashowHiggsPotentialSite(site_index);
 }
-
+//! Investigate this term
 double HiggsMixedTerm(int site_index, int dir) // dir = mu,  Tr( \Phi(x) U_{\mu}(x)  \Phi(x+\mu)  U_{\mu}^{\adjoint}(x) )
 {
     int x[4], jump_index;
