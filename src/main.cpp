8b9dea092ec22d5dc6c092f7832b2ffd5ab6d3cd
pmartin95 <martip8@rpi.edu>
2023-01-16 11:04:44 -0500

    different dat folder

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ double beta = 2.3;
 double lambda = 0.1;
 double m2 = 2.0;
 double kappa = 0.0; // This term controls the strength of the Higgs kinetic term in the action.
-int MAX_ITER = 1;
+int MAX_ITER = 10;
 int iter_count = 0;
 int Naccept = 0, Nreject = 0;
 int NacceptLink = 0, NrejectLink = 0;
@@ -43,7 +43,7 @@ std::mt19937 rng;
 std::uniform_real_distribution<double> gen(0.0, 1.0);
 std::normal_distribution<double> gen_normal(0.0, 1.0);
 boundary_condition bc = &ptwist;
-std::string datFolder = "./dat/testdata7/";
+std::string datFolder = "./dat/testdata6/";
 std::string confFolder = "./configurations/";
 std::string bcName = "p";
 std::string identifier;
@@ -52,10 +52,10 @@ int main(int argc, char **argv)
     if (!boost::filesystem::exists(boost::filesystem::path(datFolder)))
         boost::filesystem::create_directory(boost::filesystem::path(datFolder));
     // Boilerplate
-    // std::random_device r;
-    // rng.seed(r());
-    // lattice = new site[lsites];
-    // simulation1(argc, argv);
-    // delete[] lattice;
+    std::random_device r;
+    rng.seed(r());
+    lattice = new site[lsites];
+    simulation1(argc, argv);
+    delete[] lattice;
     return 0;
 }
